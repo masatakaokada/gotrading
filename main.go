55cb044553ff30567eac95230e40be511d043f85
Masataka Okada <masataka.okada1117@gmail.com>
2021-08-13 11:18:26 +0900

    ローソク足チャートのデータ収集メソッド追加

--- main.go
+++ main.go
@@ -2,26 +2,29 @@ package main
 
 import (
 	"fmt"
-	"gotrading/bitflyer"
+	"gotrading/app/controllers"
+	"gotrading/app/models"
 	"gotrading/config"
 	"gotrading/utils"
-	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	fmt.Println(models.DbConnection)
 
-	tickerChannel := make(chan bitflyer.Ticker)
-	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
-	for ticker := range tickerChannel {
-		fmt.Println(ticker)
-		fmt.Println(ticker.GetMidPrice())
-		fmt.Println(ticker.DateTime())
-		fmt.Println(ticker.TruncateDateTime(time.Second))
-		fmt.Println(ticker.TruncateDateTime(time.Minute))
-		fmt.Println(ticker.TruncateDateTime(time.Hour))
-	}
+	controllers.StreamIngestionData()
+	// apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+
+	// tickerChannel := make(chan bitflyer.Ticker)
+	// go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
+	// for ticker := range tickerChannel {
+	// 	fmt.Println(ticker)
+	// 	fmt.Println(ticker.GetMidPrice())
+	// 	fmt.Println(ticker.DateTime())
+	// 	fmt.Println(ticker.TruncateDateTime(time.Second))
+	// 	fmt.Println(ticker.TruncateDateTime(time.Minute))
+	// 	fmt.Println(ticker.TruncateDateTime(time.Hour))
+	// }
 
 	// ビットコインの買い注文を出す
 	// order := &bitflyer.Order{
