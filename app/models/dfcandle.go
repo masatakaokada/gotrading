388b92b74ba9eb0001f96eb5ce3aa4c3b39f3815
Masataka Okada <masataka.okada1117@gmail.com>
2021-08-28 10:30:44 +0900

    Macdのバックテスト追加

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -345,3 +345,55 @@ func (df *DataFrameCandle) OptimizeIchimoku() (performance float64) {
 	performance = signalEvents.Profit()
 	return performance
 }
+
+func (df *DataFrameCandle) BackTestMacd(macdFastPeriod, macdSlowPeriod, macdSignalPeriod int) *SignalEvents {
+	lenCandles := len(df.Candles)
+	if lenCandles <= macdFastPeriod || lenCandles <= macdSlowPeriod || lenCandles <= macdSignalPeriod {
+		return nil
+	}
+
+	signalEvents := &SignalEvents{}
+	outMACD, outMACDSignal, _ := talib.Macd(df.Closes(), macdFastPeriod, macdSlowPeriod, macdSignalPeriod)
+
+	for i := 1; i < lenCandles; i++ {
+		if outMACD[i] < 0 &&
+			outMACDSignal[i] < 0 &&
+			outMACD[i-1] < outMACDSignal[i-1] &&
+			outMACD[i] >= outMACDSignal[i] { // MACDがシグナルを追い越したとき
+			signalEvents.Buy(df.ProductCode, df.Candles[i].Time, df.Candles[i].Close, 1.0, false)
+		}
+
+		if outMACD[i] > 0 &&
+			outMACDSignal[i] > 0 &&
+			outMACD[i-1] > outMACDSignal[i-1] &&
+			outMACD[i] <= outMACDSignal[i] { // MACDがシグナルを下回ったとき
+			signalEvents.Sell(df.ProductCode, df.Candles[i].Time, df.Candles[i].Close, 1.0, false)
+		}
+	}
+	return signalEvents
+}
+
+func (df *DataFrameCandle) OptimizeMacd() (performance float64, bestMacdFastPeriod, bestMacdSlowPeriod, bestMacdSignalPeriod int) {
+	bestMacdFastPeriod = 12
+	bestMacdSlowPeriod = 26
+	bestMacdSignalPeriod = 9
+
+	for fastPeriod := 10; fastPeriod < 19; fastPeriod++ {
+		for slowPeriod := 20; slowPeriod < 30; slowPeriod++ {
+			for signalPeriod := 5; signalPeriod < 15; signalPeriod++ {
+				signalEvents := df.BackTestMacd(bestMacdFastPeriod, bestMacdSlowPeriod, bestMacdSignalPeriod)
+				if signalEvents == nil {
+					continue
+				}
+				profit := signalEvents.Profit()
+				if performance < profit {
+					performance = profit
+					bestMacdFastPeriod = fastPeriod
+					bestMacdSlowPeriod = slowPeriod
+					bestMacdSignalPeriod = signalPeriod
+				}
+			}
+		}
+	}
+	return performance, bestMacdFastPeriod, bestMacdSlowPeriod, bestMacdSignalPeriod
+}
